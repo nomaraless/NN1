5a046a7556e25eb3b97a5ef13a354bc41ecd2d24
nomar <nomaraless@gmail.com>
2025-02-15 23:35:27 +0300

    v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            int a = 10;
-            int b = 0;
-            int sum = a/b;
-        } catch (ArithmeticException e) {
-            System.out.println("Ошибка: делить на ноль нельзя");
-        }
     }
 }
